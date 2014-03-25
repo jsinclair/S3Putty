5604ddfbef762dacfa0da0db3b280291a6d7b48d
jsinclair <jsincl4ir@gmail.com>
2014-03-25 22:58:53 +0200

    Music!
    Initial version of the music manager, courtesy of
    http://www.rbgrn.net/content/307-light-racer-20-days-61-64-completion.

--- Stone3Putty/src/com/example/stone3putty/MainActivity.java
+++ Stone3Putty/src/com/example/stone3putty/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.stone3putty;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
+
+import com.example.stone3putty.util.MusicManager;
+import com.example.stone3putty.util.SystemUiHider;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -15,43 +18,66 @@ import android.widget.TextView;
  * @see SystemUiHider
  */
 public class MainActivity extends Activity {
+	
+	boolean continueMusic = true;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Typeface tf = Typeface.createFromAsset(getAssets(),"data/fonts/CelticHand.ttf");
+
+		Typeface tf = Typeface.createFromAsset(getAssets(),
+				"data/fonts/CelticHand.ttf");
 		((TextView) findViewById(R.id.start)).setTypeface(tf);
 		((TextView) findViewById(R.id.customise)).setTypeface(tf);
 		((TextView) findViewById(R.id.settings)).setTypeface(tf);
 		((TextView) findViewById(R.id.about)).setTypeface(tf);
 		((TextView) findViewById(R.id.exit)).setTypeface(tf);
-		
 	}
 
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (!continueMusic) {
+			MusicManager.pause();
+		}
+	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		continueMusic = false;
+		MusicManager.start(this, MusicManager.MUSIC_MENU);
+	}
+	
 	/**
-	 * This is called when the start label is clicked (as defined in activity_main.xml). 
-	 * It is meant to start a new game activity.
-	 * @param view The view that was clicked. 
+	 * This is called when the start label is clicked (as defined in
+	 * activity_main.xml). It is meant to start a new game activity.
+	 * 
+	 * @param view
+	 *            The view that was clicked.
 	 */
-	public void startGame(View view){
+	public void startGame(View view) {
+		continueMusic = true;
+		MusicManager.start(this, MusicManager.MUSIC_GAME, true);
 		Intent intent = new Intent(this, GameActivity.class);
 		startActivity(intent);
 	}
-	
-	public void startSettings(View view){
-		System.out.println("settings");
+
+	public void startSettings(View view) {
+		continueMusic = true;
 		Intent intent = new Intent(this, SettingsActivity.class);
 		startActivity(intent);
 	}
-	
-	/** 
-	 * This is called when the exit label is clicked  (as defined in activity_main.xml). 
-	 * @param view The view that was clicked.
+
+	/**
+	 * This is called when the exit label is clicked (as defined in
+	 * activity_main.xml).
+	 * 
+	 * @param view
+	 *            The view that was clicked.
 	 */
-	public void exit (View view){
+	public void exit(View view) {
 		finish();
 	}
 }

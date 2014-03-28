01cd1fec2f5eba2085463b7a8f6d1fe1446fdb50
jsinclair <james@mytcg.net>
2014-03-28 11:14:34 +0200

    VIBRATE!
    lol

--- Stone3Putty/src/com/example/stone3putty/SettingsActivity.java
+++ Stone3Putty/src/com/example/stone3putty/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.example.stone3putty;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -14,7 +16,7 @@ import com.example.stone3putty.util.MusicManager;
 public class SettingsActivity extends Activity {
 
 	private SharedPreferences.Editor editor;
-	
+
 	boolean continueMusic = true;
 
 	@Override
@@ -28,13 +30,20 @@ public class SettingsActivity extends Activity {
 		((TextView) findViewById(R.id.soundLabel)).setTypeface(tf);
 		((TextView) findViewById(R.id.musicLabel)).setTypeface(tf);
 		((TextView) findViewById(R.id.vibrateLabel)).setTypeface(tf);
-		
+
 		// Restore preferences
-		SharedPreferences settings = getSharedPreferences(this.getString(R.string.settings_storage), 0);
-		((ToggleButton) findViewById(R.id.soundToggleButton)).setChecked(settings.getBoolean(this.getString(R.string.key_sound_on), true));
-		((ToggleButton) findViewById(R.id.musicToggleButton)).setChecked(settings.getBoolean(this.getString(R.string.key_music_on), true));
-		((ToggleButton) findViewById(R.id.vibrateToggleButton)).setChecked(settings.getBoolean(this.getString(R.string.key_vibrate_on), true));
-		
+		SharedPreferences settings = getSharedPreferences(
+				this.getString(R.string.settings_storage), 0);
+		((ToggleButton) findViewById(R.id.soundToggleButton))
+				.setChecked(settings.getBoolean(
+						this.getString(R.string.key_sound_on), true));
+		((ToggleButton) findViewById(R.id.musicToggleButton))
+				.setChecked(settings.getBoolean(
+						this.getString(R.string.key_music_on), true));
+		((ToggleButton) findViewById(R.id.vibrateToggleButton))
+				.setChecked(settings.getBoolean(
+						this.getString(R.string.key_vibrate_on), true));
+
 		// Initialize the editor
 		editor = settings.edit();
 	}
@@ -64,24 +73,39 @@ public class SettingsActivity extends Activity {
 
 		return super.onKeyDown(keyCode, event);
 	}
-	
+
 	public void toggleSound(View view) {
-		editor.putBoolean(this.getString(R.string.key_sound_on), ((ToggleButton) findViewById(R.id.soundToggleButton)).isChecked());
-		
+		editor.putBoolean(this.getString(R.string.key_sound_on),
+				((ToggleButton) findViewById(R.id.soundToggleButton))
+						.isChecked());
+
 		editor.commit();
 	}
-	
+
 	public void toggleMusic(View view) {
-		editor.putBoolean(this.getString(R.string.key_music_on), ((ToggleButton) findViewById(R.id.musicToggleButton)).isChecked());
-		
+		editor.putBoolean(this.getString(R.string.key_music_on),
+				((ToggleButton) findViewById(R.id.musicToggleButton))
+						.isChecked());
+
 		editor.commit();
-		
-		MusicManager.toggleMusic(this, ((ToggleButton) findViewById(R.id.musicToggleButton)).isChecked(), MusicManager.MUSIC_MENU);
+
+		MusicManager.toggleMusic(this,
+				((ToggleButton) findViewById(R.id.musicToggleButton))
+						.isChecked(), MusicManager.MUSIC_MENU);
 	}
-	
+
 	public void toggleVibrate(View view) {
-		editor.putBoolean(this.getString(R.string.key_vibrate_on), ((ToggleButton) findViewById(R.id.vibrateToggleButton)).isChecked());
-		
+		editor.putBoolean(this.getString(R.string.key_vibrate_on),
+				((ToggleButton) findViewById(R.id.vibrateToggleButton))
+						.isChecked());
+
+		if (((ToggleButton) findViewById(R.id.vibrateToggleButton)).isChecked()) {
+			Vibrator v = (Vibrator) this
+					.getSystemService(Context.VIBRATOR_SERVICE);
+			// Vibrate for 250 milliseconds
+			v.vibrate(250);
+		}
+
 		editor.commit();
 	}
 }
